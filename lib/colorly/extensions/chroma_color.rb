25db11415c42113ce0dd698c971f9feb63673fe5
Danny Ben Shitrit <db@dannyben.com>
2021-06-18 10:57:25 +0000

    refactor chroma naming functions

--- lib/colorly/extensions/chroma_color.rb
+++ lib/colorly/extensions/chroma_color.rb
@@ -16,12 +16,9 @@ protected
   end
 
   def name_info!
-    r, g, b = rgb
-    h, s, l = hsl
-    ndf1 = 0
-    ndf2 = 0
-    ndf = 0
-    df = nil
+    rgb1 = rgb
+    hsl1 = hsl
+    distance = nil
     result = nil
 
     Colorly.color_names.each do |this_hex, this_name, this_shade|
@@ -33,20 +30,12 @@ protected
         }
       end
 
-      r2, g2, b2 = this_hex.paint.rgb.to_a
-      h2, s2, l2 = this_hex.paint.hsl.to_a
+      rgb2 = this_hex.paint.rgb.to_a
+      hsl2 = this_hex.paint.hsl.to_a
+      candidate_distance = color_distance rgb1, hsl1, rgb2, hsl2
 
-      ndf1 = (r - r2) ** 2 + 
-             (g - g2) ** 2 + 
-             (b - b2) ** 2
-      ndf2 = (((h - h2) ** 2).abs / 360.0) + 
-             ((s - s2) ** 2).abs + 
-             ((l - l2) ** 2).abs
-
-      ndf = ndf1 + ndf2 * 2
-
-      if !df or df > ndf
-        df = ndf
+      if !distance or distance > candidate_distance
+        distance = candidate_distance
         result = {
           name: this_name, 
           shade: this_shade,
@@ -55,5 +44,25 @@ protected
     end
 
     result
+  end
+
+  def color_distance(rgb1, hsl1, rgb2, hsl2)
+    rgb_distance(rgb1, rgb2) + hsl_distance(hsl1, hsl2) * 2
+  end
+
+  def rgb_distance(rgb1, rgb2)
+    r1, g1, b1 = rgb1
+    r2, g2, b2 = rgb2
+
+    (r1 - r2) ** 2 + (g1 - g2) ** 2 + (b1 - b2) ** 2
+  end
+
+  def hsl_distance(hsl1, hsl2)
+    h1, s1, l1 = hsl1
+    h2, s2, l2 = hsl2
+
+    (((h1 - h2) ** 2).abs / 360.0) + 
+      ((s1 - s2) ** 2).abs + 
+      ((l1 - l2) ** 2).abs
   end
 end
